a2c8a3397510c9ea0393bba63f9b461648d2372c
daniel m <daniel.martinezvillegas2001@gmail.com>
2020-06-19 10:19:19 -0500

    alo

---
+++ index.js
@@ -0,0 +1,51 @@
+var elementos= document.querySelectorAll('.nume');
+var sumando = document.querySelector('.suma');  
+var restando = document.querySelector('.resta');  
+var igual = document.querySelector('.igual');  
+var respuesta = document.querySelector('.respuesta'); 
+var nume1 = ''; 
+var nume2 = ''; 
+var sumatoria = false; 
+var cambio = false; 
+var resta  = false; 
+var  asies; 
+for (let index = 0; index < elementos.length; index++) {
+  elementos[index].addEventListener('click', ()=>{  
+    if(!cambio){
+      nume1 = ''; 
+      respuesta.innerHTML= nume1 + elementos[index].innerHTML;
+      nume1= nume1 + elementos[index].innerHTML; 
+      console.log(nume1);
+    }else{
+      nume2 = '';
+      respuesta.innerHTML= nume2 + elementos[index].innerHTML;
+      nume2= nume2 + elementos[index].innerHTML; 
+      console.log(nume2+'aloi');
+    }
+  }); 
+}
+//...........operaciones
+sumando.addEventListener('click', ()=>{
+  cambio= true; 
+  sumatoria= true; 
+}); 
+restando.addEventListener('click', ()=>{
+  alert('hola restando'); 
+});
+igual.addEventListener('click', ()=>{
+  var primero = parseInt(nume1); 
+  var segundo = parseInt(nume2); 
+  if(sumatoria){
+    asies= suma(primero, segundo);
+  }
+  respuesta.innerHTML= asies; 
+  cambio = false; 
+  sumatoria= false; 
+});
+
+respuesta.innerHTML= ''; 
+
+function suma (a,b){
+
+  return a+b; 
+};
